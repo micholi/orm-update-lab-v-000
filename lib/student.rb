b933f6a087970538ace93c8e6a288236bedd38a1
Michelle Olivieri <michelleo72@gmail.com>
2018-04-27 15:07:25 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -55,7 +55,7 @@ class Student
     new_student = self.new
     new_student.id = row[0]
     new_student.name = row[1]
-    new_student.name = row[2]
+    new_student.grade = row[2]
   end
 
   def self.find_by_name(name)
