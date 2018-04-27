a28ee250d55cbdd72a7249e46f71cc651d7e98e2
Michelle Olivieri <michelleo72@gmail.com>
2018-04-27 15:03:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,8 +58,15 @@ class Student
     new_student.name = row[2]
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
 
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
   end
 
   def update
