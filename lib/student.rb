8bad527627dcfd1b1c12d1c04ef2a27e92ae99bd
Michelle Olivieri <michelleo72@gmail.com>
2018-04-27 14:39:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,4 +14,32 @@ class Student
     @id = id
   end
 
+  def self.create_table
+
+  end
+
+  def self.drop_table
+
+  end
+
+  def save
+
+  end
+
+  def self.create
+
+  end
+
+  def self.new_from_db
+
+  end
+
+  def self.find_by_name
+
+  end
+
+  def update
+
+  end
+
 end
